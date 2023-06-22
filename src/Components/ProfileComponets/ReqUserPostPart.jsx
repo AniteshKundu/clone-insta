e8f24e3e0dfe54d0da60f91747972fc2cc3cffd9
AniteshKundu <aniteshkundu.cse2020@nsec.ac.in>
2023-06-23 03:06:21 +0530

    Commit day2

--- src/Components/ProfileComponets/ReqUserPostPart.jsx
+++ src/Components/ProfileComponets/ReqUserPostPart.jsx
@@ -1,41 +1,49 @@
 import React, { useState } from 'react'
 import { AiOutlineTable, AiOutlineUser } from 'react-icons/ai'
-import { BiBookmark } from "react-icons/bi"
 import { RiVideoAddLine } from 'react-icons/ri'
+import {BiBookmark} from 'react-icons/bi'
+import ReqUserPostCard from './ReqUserPostCard'
 
 const ReqUserPostPart = () => {
-    const [activeTab,setActiveTab]=useState()
+    const [activeTab, setActiveTab] = useState()
     const tabs=[
         {
-            tab: "Post",
-            icon:<AiOutlineTable></AiOutlineTable>,
-            activeTab:""
+        tab:"Posts",
+        icon:<AiOutlineTable></AiOutlineTable>,
+        activeTab:""
         },
         {
-            tab: "Reels",
-            icon:<RiVideoAddLine/>
+            tab:"Reels",
+            icon:<RiVideoAddLine></RiVideoAddLine>
         },
         {
-            tab: "Saved",
-            icon:<BiBookmark/>,
+            tab:"Saved",
+            icon:<BiBookmark></BiBookmark>
         },
         {
-            tab: "Tagged",
-            icon:<AiOutlineUser/>
+            tab:"Tagged",
+            icon:<AiOutlineUser></AiOutlineUser>
         }
-    ] 
-
-  return (
-    <div>
-        <div className='flex space-x-14 border-t relative '>
-            {tabs.map((items)=>  <div onClick={()=>setActiveTab(items.tab)} ClassName={'${activeTab===items.tab?"border-t border-black":"opacity-60"} flex items-center cursor-pointer py-2 text-sm'}>
-                <p> {items.icon}</p>
-                <p className='ml-1'> {items.tab}</p>
+    ]
+    return (
+        <div>
+            <div className='flex space-x-14 border-t relative'>
+                {tabs.map((item)=> 
+                (
+                    <div onClick={()=>setActiveTab(item.tab)} className={`${activeTab === item.tab?"border-t border-black":"opacity-60"} flex items-center cursor-pointer`}>
+                    <p>{item.icon}</p>
+                    <p className='ml-1'>{item.tab}</p>
+                </div>
+                ))}
             </div>
-            )}
+            <div>
+                <div className='flex flex-wrap'>
+                    {[1,,1,,1,1,1,1].map((item)=><ReqUserPostCard/>)}
+                </div>
+            </div>
+            
         </div>
-    </div>
-  )
+    )
 }
 
 export default ReqUserPostPart
